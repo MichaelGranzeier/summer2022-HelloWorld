e05c11b629f5347f1de9b1fa70524331193004ba
MichaelGranzeier <granzeierpeter@gmail.com>
2022-05-18 14:38:56 -0400

    wariable notes

---
+++ Summer2022-HelloWorld/Summer2022-HelloWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// variable explanation
+// what is a variable?
+// named objests . value that can change
+// variables are named objects with values and types
+// instructor has a value of davis
+// instructor has a value of gavin
+// type name;
+string instructor; //this variable is 'null'
+// name = value;
+instructor = "davis";
+instructor = "gavin";
+Console.WriteLine(instructor);
+
+//type name = value;
+string coInstructor = "gavin";
+
+int studentCount = 8;
+string input = Console.ReadLine();
+Console.WriteLine(input);
+
+int num = 5 + 5;
+Console.WriteLine(num);
+
+//"" -> String
+// d,D,.0 -> double
+// f,F -> float
+// m,M -> decimal
+// true,false -> boolean
+// number -> int
+// '' -> char
